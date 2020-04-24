18a83fecf3be235c953157529bbf4475caf69716
ABC <ABC@DESKTOP-UOHKJEK>
2020-04-24 19:11:02 -0400

    commit

---
+++ PecuniaBanking-Shanthi/src/main/java/com/capg/pecunia/entity/AccountBean.java
@@ -0,0 +1,69 @@
+package com.capg.pecunia.entity;
+
+
+public class AccountBean {
+	String accountNumber;
+	String customerFirstName;
+	String cutomerLastName;
+	double accountBalance;
+	String customerAadhar;
+	String cutomerPan;
+	String cutomerDob;
+	String customerContact;
+	AccountBean()
+	{
+		super();
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getCustomerFirstName() {
+		return customerFirstName;
+	}
+	public void setCustomerFirstName(String customerFirstName) {
+		this.customerFirstName = customerFirstName;
+	}
+	public String getCutomerLastName() {
+		return cutomerLastName;
+	}
+	public void setCutomerLastName(String cutomerLastName) {
+		this.cutomerLastName = cutomerLastName;
+	}
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	public String getCustomerAadhar() {
+		return customerAadhar;
+	}
+	public void setCustomerAadhar(String customerAadhar) {
+		this.customerAadhar = customerAadhar;
+	}
+	public String getCutomerPan() {
+		return cutomerPan;
+	}
+	public void setCutomerPan(String cutomerPan) {
+		this.cutomerPan = cutomerPan;
+	}
+	public String getCutomerDob() {
+		return cutomerDob;
+	}
+	public void setCutomerDob(String cutomerDob) {
+		this.cutomerDob = cutomerDob;
+	}
+	
+	
+	public String getCustomerContact() {
+		return customerContact;
+	}
+	public void setCustomerContact(String customerContact) {
+		this.customerContact = customerContact;
+	}
+	
+}
+	
