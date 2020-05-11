2e2c1f75672574156282635a84f4cf786e493a06
dell <dell@DESKTOP-LBU2II5>
2020-05-11 20:31:12 +0530

    commit

---
+++ src/main/java/com/capg/pecunia/service/IAccountService.java
@@ -0,0 +1,18 @@
+package com.capg.pecunia.service;
+
+import java.util.List;
+
+import com.capg.pecunia.entity.AccountBean;
+
+public interface IAccountService  {
+	
+	public AccountBean addAccount(AccountBean bean);
+	
+	public AccountBean updateAccount(AccountBean bean,long accNumber); 
+	
+	public void deleteAccount(long accNumber);
+
+    public List<AccountBean> viewAll();
+	
+
+}
